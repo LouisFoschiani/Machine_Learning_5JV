071e84aab68f589bfed6f984b21abfbf17af78f0
LouisF <54190296+LouisFoschiani@users.noreply.github.com>
2023-10-10 23:01:29 +0200

    petit code de test avec le pire langage du monde

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![allow(non_snake_case)]
+
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    // --snip--
+
+    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret Number:{secret_number}");
+    println!("Guess the number!");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+
+}
