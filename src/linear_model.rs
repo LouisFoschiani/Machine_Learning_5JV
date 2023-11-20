151f9f89ed3cd03a380eb4f7a043685fa69a6a88
LouisF <54190296+LouisFoschiani@users.noreply.github.com>
2023-11-20 22:17:05 +0100

    update try to fix

--- src/linear_model.rs
+++ src/linear_model.rs
@@ -1,15 +1,11 @@
 extern crate ndarray;
 extern crate image;
 
-use ndarray::{Array, Array1, Array2};
-use image::{DynamicImage, GenericImageView, ImageError, open};
-use std::fs;
-use std::path::Path;
+use ndarray::{Array1, Array2};
+use image::{GenericImageView, ImageError, io::Reader as ImageReader};
+use std::fs::{self, File};
+use std::io::{Write, BufWriter};
 use rand::Rng;
-use std::fmt;
-use image::io::Reader as ImageReader;
-
-// Structure du modèle linéaire
 
 struct LinearModel {
     weights: Array1<f32>,
@@ -23,9 +19,10 @@ impl LinearModel {
     }
 
     fn train(&mut self, inputs: &Array2<f32>, labels: &Array1<i32>, iterations: usize, learning_rate: f32) {
+        let labels_f32 = labels.mapv(|l| l as f32);
         for _ in 0..iterations {
             let predictions = inputs.dot(&self.weights);
-            let errors = &labels - &predictions;
+            let errors = &labels_f32 - &predictions;
             self.weights += &(inputs.t().dot(&errors) * learning_rate);
         }
     }
@@ -33,38 +30,54 @@ impl LinearModel {
     fn predict(&self, input: &Array1<f32>) -> i32 {
         if input.dot(&self.weights) > 0.5 { 1 } else { 0 }
     }
+
+    fn save_weights(&self, file_path: &str) -> std::io::Result<()> {
+        let file = File::create(file_path)?;
+        let mut writer = BufWriter::new(file);
+
+        for weight in self.weights.iter() {
+            writeln!(writer, "{}", weight)?;
+        }
+
+        Ok(())
+    }
+
+    fn predict_category(&self, input: &Array1<f32>) -> String {
+        match self.predict(input) {
+            0 => "Banane".to_string(),
+            1 => "Avocat".to_string(),
+            2 => "Tomate".to_string(),
+            _ => "Inconnu".to_string(),
+        }
+    }
 }
 
-fn load_image(path: &str) -> Vec<f32> {
-    let img = ImageReader::open(path).unwrap().decode().unwrap();
-    img.pixels()
-        .map(|p| p.2[0] as f32 / 255.0)
-        .collect::<Vec<f32>>()
+fn load_image(path: &str) -> Result<Vec<f32>, ImageError> {
+    let img = ImageReader::open(path)?.decode()?;
+    Ok(img.pixels().map(|p| p.2[0] as f32 / 255.0).collect())
 }
 
-fn load_images_from_folder(folder: &str) -> Vec<Vec<f32>> {
-    fs::read_dir(folder).unwrap()
-        .filter_map(|entry| {
-            entry.ok().and_then(|e| {
-                let path = e.path();
-                if path.is_file() {
-                    Some(load_image(path.to_str().unwrap()))
-                } else {
-                    None
-                }
-            })
-        })
-        .collect()
+fn load_images_from_folder(folder: &str) -> Result<Vec<Vec<f32>>, Box<dyn std::error::Error>> {
+    let mut images = Vec::new();
+    let read_dir = fs::read_dir(folder).map_err(|e| format!("Erreur lors de la lecture du dossier '{}': {}", folder, e))?;
+
+    for entry in read_dir {
+        let path = entry.map_err(|e| format!("Erreur lors de l'accès à une entrée de dossier: {}", e))?.path();
+        if path.is_file() {
+            images.push(load_image(path.to_str().ok_or_else(|| "Chemin non valide".to_string())?)?);
+        }
+    }
+    Ok(images)
 }
 
 fn create_labels(num_images: usize, label: i32) -> Vec<i32> {
     vec![label; num_images]
 }
 
-fn main() {
-    let banana_images = load_images_from_folder("Unknown/Banana");
-    let avocado_images = load_images_from_folder("Unknown/Avocado");
-    let tomato_images = load_images_from_folder("Unknown/Tomato");
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let banana_images = load_images_from_folder("C:/chemin/complet/vers/Unknown/Banana")?;
+    let avocado_images = load_images_from_folder("Unknown/Avocado")?;
+    let tomato_images = load_images_from_folder("Unknown/Tomato")?;
 
     let banana_labels = create_labels(banana_images.len(), 0);
     let avocado_labels = create_labels(avocado_images.len(), 1);
@@ -74,9 +87,21 @@ fn main() {
     let all_labels = [banana_labels, avocado_labels, tomato_labels].concat();
 
     let flattened_images: Vec<f32> = all_images.into_iter().flatten().collect();
-    let inputs = Array2::from_shape_vec((all_labels.len(), flattened_images.len() / all_labels.len()), flattened_images).unwrap();
+    let inputs = Array2::from_shape_vec((all_labels.len(), flattened_images.len() / all_labels.len()), flattened_images)?;
     let labels = Array1::from(all_labels);
 
     let mut model = LinearModel::new(inputs.ncols());
     model.train(&inputs, &labels, 1000, 0.01);
+
+    // Sauvegarde des poids du modèle
+    model.save_weights("model_weights.txt")?;
+
+    // Exemple de prédiction (à adapter selon vos besoins)
+    // Supposons que vous ayez une image "path/to/new_image.jpg"
+    // let new_image = load_image("path/to/new_image.jpg")?;
+    // let new_image_vector = Array1::from(new_image);
+    // let category = model.predict_category(&new_image_vector);
+    // println!("Catégorie prédite: {}", category);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 #![allow(non_snake_case)]
 mod linear_model;
-//mod mlp;
-//mod rbf;
 
 fn main() {
-    let algorithm_choice = "linear_model";
-
-    match algorithm_choice {
-        "linear_model" => {
-            println!("Running Linear Model Algorithm...");
-            linear_model::main().unwrap(); // Remarquez le changement de nom de la fonction
-        }
-        /* "mlp" => {
-             println!("Running Multi-Layer Perceptron Algorithm...");
-             mlp::run_mlp_algorithm(); // Changez le nom de la fonction en fonction de l'algorithme
-         }
-         "rbf" => {
-             println!("Running Radial Basis Function Network Algorithm...");
-             rbf::run_rbf_algorithm(); // Changez le nom de la fonction en fonction de l'algorithme
-         }*/
-        _ => {
-            println!("Unknown algorithm choice.");
-        }
+    match linear_model::run() {
+        Ok(()) => println!("Linear model executed successfully."),
+        Err(e) => println!("Error executing linear model: {}", e),
     }
 }
