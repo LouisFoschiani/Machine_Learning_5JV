9f8a1c9c9f7b3160b393d68f8b40aebb4a77eecb
JohnPoliakov <punishercinq@gmail.com>
2023-12-15 02:39:57 +0100

    Linear Model

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn load_image_data(base_path: &Path, target_category: &str, non_target_category:
         if path.is_file() {
             let image_features = process_image(&path)?;
             features.push(image_features);
-            labels.push(1); // Label 1 for target category
+            labels.push(1);
         }
     }
 
@@ -31,7 +31,7 @@ fn load_image_data(base_path: &Path, target_category: &str, non_target_category:
         if path.is_file() {
             let image_features = process_image(&path)?;
             features.push(image_features);
-            labels.push(0); // Label 0 for non-target category
+            labels.push(-1);
         }
     }
 
@@ -57,48 +57,37 @@ fn init_model_weights(cols_x_len: usize, rows_w_len: usize) -> Vec<f32> {
     weights
 }
 
-fn predict_linear_model_score(model_weights: &[f32], inputs: &[f32]) -> f32 {
+fn predict_linear_model_classification(model_weights: &[f32], inputs: &[f32]) -> i32 {
     let mut res = 0.0;
     for (&weight, &input) in model_weights.iter().skip(1).zip(inputs.iter()) {
         res += weight * input;
     }
-    model_weights[0] + res
+    let total_sum = model_weights[0] + res;
+    if total_sum >= 0.0 {
+        1
+    } else {
+        -1
+    }
 }
 
-fn train_linear_model(x: &[Vec<f32>], y: &[i32], w: &mut [f32], rows_x_len: usize, rows_w_len: usize, iter: usize) -> f32 {
+fn train_linear_model(x: &[Vec<f32>], y: &[i32], w: &mut [f32], rows_x_len: i32, rows_w_len: i32, iter: i32) {
     let learning_rate = 0.001;
-    let mut last_cost = f32::MAX;
 
     for _ in 0..iter {
-        let k = rand::thread_rng().gen_range(0..rows_x_len);
-        let gxk = predict_linear_model_score(w, &x[k]);
-        let yk = y[k];
-        let diff = yk as f32 - gxk;
+        let k = rand::thread_rng().gen_range(0..rows_x_len) as usize;
+        let gxk = predict_linear_model_classification(w, &x[k]) as f32;
+        let yk = y[k] as f32;
+        let diff = yk - gxk;
 
         w[0] = w[0] + learning_rate * diff;
-        for j in 1..rows_w_len {
+        for j in 1..rows_w_len as usize {
             w[j] = w[j] + learning_rate * diff * x[k][j - 1];
         }
-
-        // Calculating mean squared error
-        let cost = x.iter().zip(y.iter())
-            .map(|(features, &label)| {
-                let prediction_score = predict_linear_model_score(w, features);
-                (label as f32 - prediction_score).powi(2)
-            })
-            .sum::<f32>() / x.len() as f32;
-
-        // Check for convergence
-        if cost > last_cost || (last_cost - cost).abs() < 1e-5 {
-            break;
-        }
-        last_cost = cost;
     }
-
-    last_cost
 }
 
-fn save_model_linear(model_weight: &[f32], file_path: &Path, efficiency: f64) -> Result<(), io::Error> {
+
+fn save_model_linear(model_weight: &[f32], file_path: &Path, efficiency: f32) -> Result<(), io::Error> {
     let mut file = File::create(file_path)?;
     writeln!(file, "-- Efficiency --\n{}", efficiency)?;
     writeln!(file, "-- Weights --")?;
@@ -108,84 +97,143 @@ fn save_model_linear(model_weight: &[f32], file_path: &Path, efficiency: f64) ->
     Ok(())
 }
 
-fn load_model_weights(file_path: &Path) -> io::Result<(Vec<f32>, f64)> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
+fn load_model_weights(file_path: &Path, colX: i32, rowW: i32) -> io::Result<(Vec<f32>, f32)> {
+
     let mut weights = Vec::new();
     let mut efficiency = 0.0;
-    let mut read_efficiency = false;
 
-    for line in reader.lines() {
-        let line = line?;
-        if line.starts_with("-- Efficiency --") {
-            read_efficiency = true;
-            continue;
-        }
+    if fs::metadata(file_path).is_ok() {
 
-        if read_efficiency {
-            efficiency = line.parse::<f64>().unwrap_or_else(|_| 0.0);
-            read_efficiency = false;
-            continue;
-        }
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+        let mut read_efficiency = false;
 
-        if line.starts_with("{") && line.ends_with("}") {
-            let weight_str = &line[1..line.len()-1];
-            match weight_str.trim().parse::<f32>() {
-                Ok(weight) => weights.push(weight),
-                Err(_) => continue,
+        for line in reader.lines() {
+            let line = line?;
+            if line.starts_with("-- Efficiency --") {
+                read_efficiency = true;
+                continue;
+            }
+
+            if read_efficiency {
+                efficiency = line.parse::<f32>().unwrap_or_else(|_| 0.0);
+                read_efficiency = false;
+                continue;
+            }
+
+            if line.starts_with("{") && line.ends_with("}") {
+                let weight_str = &line[1..line.len()-1];
+                match weight_str.trim().parse::<f32>() {
+                    Ok(weight) => weights.push(weight),
+                    Err(_) => continue,
+                }
             }
         }
+    } else {
+        weights = init_model_weights(colX as usize, rowW as usize)
     }
+
+
 
     Ok((weights, efficiency))
 }
 
+fn set_var(x: &Vec<Vec<f32>>) -> (i32, i32, i32) {
+    let rows_x_len = x.len();
+    let cols_x_len = if !x.is_empty() { x[0].len() } else { 0 };
+    let rows_w_len = cols_x_len + 1;
+    (rows_x_len as i32, cols_x_len as i32, rows_w_len as i32)
+}
+
+
 fn main() -> io::Result<()> {
-    let iterations = 10;
-    let weights_file_path = Path::new("G:\\Machine_Learning_5JV\\model_weights_Banana.txt");
 
-    let mut last_efficiency = if weights_file_path.exists() {
-        let (_, efficiency) = load_model_weights(weights_file_path)?;
-        efficiency
-    } else {
-        0.0
-    };
+    let CHECK = true;
 
-    for iter in 0..iterations {
-        println!("----------------------");
-        println!("Iteration: {}", iter + 1);
-        let base_training_path = Path::new("images/Training");
-        let base_testing_path = Path::new("images/Test");
-        let target_category = "Banana"; // Specify your target category
-        let non_target_category = "NonBanana"; // Specify your non-target category
+    let iterations = 150;
+    let weights_file_path = Path::new("model_weights.txt");
 
-        let (train_features, train_labels) = load_image_data(base_training_path, target_category, non_target_category)?;
-        let (test_features, test_labels) = load_image_data(base_testing_path, target_category, non_target_category)?;
+    let base_training_path = Path::new("images/Training");
+    let base_test_path = Path::new("images/Test");
+    let target_category = "Tomato";
+    let non_target_category = "Banana";
 
-        let rows_x_len = train_features.len();
-        let rows_w_len = train_features[0].len();
-        let mut weights = init_model_weights(rows_x_len, rows_w_len);
+    if CHECK == true{
 
-        let final_cost = train_linear_model(&train_features, &train_labels, &mut weights, rows_x_len, rows_w_len, 1000);
-        println!("Final Cost: {}", final_cost);
+        let image_path = Path::new("images\\CHECK\\Banana\\C.png"); // Chemin vers l'image à tester
 
-        // Evaluate model using Mean Squared Error
-        let total_error: f32 = test_features.iter().zip(test_labels.iter())
-            .map(|(features, &label)| {
-                let prediction_score = predict_linear_model_score(&weights, features);
-                (label as f32 - prediction_score).powi(2)
-            })
-            .sum();
-        let mean_squared_error = total_error / test_features.len() as f32;
-        println!("Mean Squared Error: {}", mean_squared_error);
+        let (train_features, _) = load_image_data(base_training_path, target_category, non_target_category)?;
 
-        if mean_squared_error < last_efficiency as f32 {
-            save_model_linear(&weights, weights_file_path, mean_squared_error as f64)?;
-            println!("Weights saved in model_weights_Banana.txt");
-            last_efficiency = mean_squared_error as f64;
-        }else{
-            println!("NO SAVE {} : {}", mean_squared_error, last_efficiency);
+        let (_, colsXLen, rowsWLen) = set_var(&train_features);
+        let (w, _) = load_model_weights(weights_file_path, colsXLen, rowsWLen)?;
+
+        // Traitement de l'image à tester
+        let image_features = process_image(&image_path)?;
+
+        // Faire une prédiction
+        let prediction = predict_linear_model_classification(&w, &image_features);
+
+        // Afficher le résultat
+        if prediction == 1 {
+            println!("C'est une TOMATE");
+        } else {
+            println!("C'est une BANANE");
+        }
+
+
+    }else{
+        for iter in 0..iterations {
+
+            println!("------- ITERATION {} -------", iter);
+            println!("\n");
+
+            // TRAINING
+
+            let (train_features, train_labels) = load_image_data(base_training_path, target_category, non_target_category)?;
+
+            let (rowsXLen, colsXLen, rowsWLen) = set_var(&train_features);
+
+            let (mut w, max_percent) = load_model_weights(weights_file_path, colsXLen, rowsWLen)?;
+
+            train_linear_model(&train_features, &train_labels, &mut w, rowsXLen, rowsWLen, 1000);
+
+            let mut final_result = 0;
+
+            for i in 0..rowsXLen {
+                let result = predict_linear_model_classification(&w, &train_features[i as usize]);
+                if result == train_labels[i as usize] {
+                    final_result += 1
+                }
+            }
+            println!("Training Result : {} / {} = {}%", final_result, train_labels.len(), final_result as f32 / train_labels.len() as f32 * 100.0);
+
+
+            // TESTING
+            final_result = 0;
+            let (train_features, train_labels) = load_image_data(base_test_path, target_category, non_target_category)?;
+            let (rowsXLen, colsXLen, rowsWLen) = set_var(&train_features);
+
+            let mut final_result = 0;
+
+            for i in 0..rowsXLen {
+                let result = predict_linear_model_classification(&w, &train_features[i as usize]);
+                if result == train_labels[i as usize] {
+                    final_result += 1
+                }
+            }
+
+            println!("Test Result : {} / {} = {}%", final_result, train_labels.len(), final_result as f32 / train_labels.len() as f32 * 100.0);
+            let success = final_result as f32 / train_labels.len() as f32 * 100.0;
+            if success > max_percent{
+                println!("Update weights : {} > {}", success, max_percent);
+                save_model_linear(&w, weights_file_path, success).expect("TODO: Could not save weights");
+            }
+
+            println!("\n");
+
         }
     }
+
+
     Ok(())
 }
